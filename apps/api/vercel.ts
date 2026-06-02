679dea81ad32d68c145f43c1e90ce96bffa7f9e3
Ru Chern CHONG <ruchern.chong@tartinerlabs.com>
2026-06-03 00:57:43 +0800

    chore: set vercel build commands and output

--- apps/api/vercel.ts
+++ apps/api/vercel.ts
@@ -1,6 +1,8 @@
 import type { VercelConfig } from "@vercel/config/v1";
 
 export const config: VercelConfig = {
+  buildCommand: "pnpm build",
+  outputDirectory: ".output",
   // Temporary disabled due to Vercel Hobby account
   // crons: [
   //   {
